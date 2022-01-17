1846d935fdc59e5cedfae526ade95d74a5e7a203
vanbeta <deimosbeta@gmail.com>
2022-01-17 12:34:32 +0530

    Initial Commit

---
+++ PlantsManager/PlantsManagerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlantsManagerViewController.swift
+//  PlantsManager
+//
+//  Created by Ivan Poderegin on 13.01.2022.
+//
+
+import UIKit
+
+class PlantsManagerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
